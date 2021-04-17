c82b486f18bf1d85d287345dd390e37f3034f7cf
Jarauta1 <you@example.com>
2021-04-17 20:11:04 +0200

    Correccion Avatar

--- src/containers/intro/Intro.js
+++ src/containers/intro/Intro.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   },
   typedContainer: {
     position: "absolute",
-    marginTop: "40px",
+    marginTop: "150px",
     /* top: "40%", */
     /* left: "50%", */
    /*  transform: "translate(-50%,-50%)", */
